0413d5e1fd96b02144d5cdbf954c67616bd8e781
James_haobam <jameskumar@agobyte.com>
2024-02-04 17:46:21 +0530

    Qpaper 2024

---
+++ home/choices.py
@@ -0,0 +1,38 @@
+trade_choices = {
+
+    'CSE':'CSE',
+    'CE': 'CE',
+    'ECE': 'ECE',
+    'ME': 'ME',
+
+}
+sem_choices = {
+
+    '1st':'1st SEM',
+    '2nd': '2nd SEM',
+    '3rd': '3rd SEM',
+    '4th': '4th SEM',
+    '5th': '5th SEM',
+    '6th': '6th SEM',
+    '7th': '7th SEM',
+    '8th': '8th SEM',
+}
+exm_choices = {
+    '1 unit':'1st unit test',
+    '2 unit':'2nd unit test',
+    'Semester':'semester exam',
+
+}
+subject_choices = {
+    "Maths":"Maths",
+    "Physics":"Physics",
+    "Chemistry":"Chemistry",
+    "Computer Science":"Computer Science",
+    "BEEE":"BEEE",
+    "ME":"Mechanical",
+    "PC":"Professional communication",
+    "FEE":"Fundamental of earthquake engg",
+    "Engg Graphics":"Engg Graphics",
+    "Technical Eng":"Technical Eng",
+    "Engg eco":"Engg economics",
+}
